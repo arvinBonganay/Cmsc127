c59f68746aa9971fa97c613451daaf25b6a3274b
arvinBonganay <arvinb450@gmail.com>
2017-04-16 11:47:29 +0800

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,172 @@
+package db;
+
+
+import java.sql.*;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame {
+    
+    private JLabel jLabel1;
+    private JLabel jLabel2;
+    private JButton loginButton;
+    private JPasswordField passwordField;
+    private JButton signUpButton;
+    private JTextField usernameTextField;
+    
+    public Login() {
+        initComponents();
+    }
+    
+    private void initComponents() {
+
+        loginButton = new JButton();
+        signUpButton = new JButton();
+        usernameTextField = new JTextField();
+        jLabel1 = new JLabel();
+        passwordField = new JPasswordField();
+        jLabel2 = new JLabel();
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        loginButton.setText("Log in");
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                loginButtonActionPerformed(evt);
+            }
+        });
+
+        signUpButton.setText("Sign Up");
+
+        signUpButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                signUpButtonActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Times New Roman", 0, 18)); 
+        jLabel1.setText("Password");
+
+        jLabel2.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+        jLabel2.setText("Username");
+
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(73, 73, 73)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addGroup(GroupLayout.Alignment.TRAILING, layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(jLabel1)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(passwordField))
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(jLabel2)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(usernameTextField, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 123, GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                            .addComponent(loginButton, GroupLayout.PREFERRED_SIZE, 91, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(signUpButton, GroupLayout.PREFERRED_SIZE, 91, GroupLayout.PREFERRED_SIZE))
+                        .addGap(69, 69, 69)))
+                .addGap(74, 74, 74))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(36, 36, 36)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(usernameTextField, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(passwordField, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
+                .addComponent(loginButton, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(signUpButton, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
+                .addGap(36, 36, 36))
+        );
+
+        pack();
+    }                        
+
+    private Connection connect(){
+        String url = "jdbc:sqlite:C:\\Users\\user\\Desktop\\dataBase\\src\\database\\data.db";
+        Connection con = null;
+        try{
+            Class.forName("org.sqlite.JDBC");
+            con = DriverManager.getConnection(url); 
+        } catch (ClassNotFoundException | SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return con;
+    }
+    
+    private void loginButtonActionPerformed(java.awt.event.ActionEvent evt) {                                            
+        // log in
+        String userName1 = usernameTextField.getText();
+        String password1 = new String(passwordField.getPassword());
+        String sql = "Select username, password From accounts Where username == ? AND password == ?";
+        try (Connection con = connect();
+            PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+            pstmt.setString(1, userName1);
+            pstmt.setString(2, password1);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()){
+                String userName2 = rs.getString("username");
+                String password2 = rs.getString("password");
+                System.out.println(userName2 + "   " + password2);
+                if (userName2.equals(userName1) && password1.equals(password2)){
+                    // log in succesful
+                    JOptionPane.showMessageDialog(null, "Log In successful");
+                    if (userName2.equals("admin")){
+                        this.dispose();
+                        new admin().setVisible(true);
+                    }
+                    return;
+                }
+            }
+            JOptionPane.showMessageDialog(null, "Account does not exist", "", JOptionPane.ERROR_MESSAGE);
+            rs.close();
+            pstmt.close();
+            con.close();
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }                                           
+
+
+    private void signUpButtonActionPerformed(java.awt.event.ActionEvent evt) {                                             
+        // open sign up window
+
+        SignUp s = new SignUp();
+        
+        s.setVisible(true);
+        usernameTextField.setText("");
+        usernameTextField.setText("");
+        passwordField.setText("");
+        passwordField.setText("");
+        this.setEnabled(false);
+        if (!s.isActive()){
+            this.setEnabled(true);
+        }
+    }                                            
+
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+    }
+                   
+}
---
+++ SignUp.java
@@ -0,0 +1,198 @@
+package db;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle;
+import javax.swing.WindowConstants;
+
+
+
+public class SignUp extends JFrame {
+    
+    private JButton jButton3;
+    private JLabel jLabel3;
+    private JLabel jLabel4;
+    private JLabel jLabel5;
+    private JLabel jLabel6;
+    private JPasswordField jPasswordField2;
+    private JPasswordField jPasswordField3;
+    private JTextField jTextField2;
+    private JTextField jTextField3;
+    
+    public SignUp() {
+        initComponents();
+    }
+
+    
+    private void initComponents() {
+
+        jLabel3 = new JLabel();
+        jLabel4 = new JLabel();
+        jLabel5 = new JLabel();
+        jTextField2 = new JTextField();
+        jTextField3 = new JTextField();
+        jPasswordField2 = new JPasswordField();
+        jPasswordField3 = new JPasswordField();
+        jLabel6 = new JLabel();
+        jButton3 = new JButton();
+
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        jLabel3.setFont(new Font("Times New Roman", 0, 16)); // NOI18N
+        jLabel3.setText("Name");
+
+        jLabel4.setFont(new Font("Times New Roman", 0, 16)); // NOI18N
+        jLabel4.setText("Username");
+
+        jLabel5.setFont(new Font("Times New Roman", 0, 16)); // NOI18N
+        jLabel5.setText("Confirm Password");
+
+        jTextField2.setFont(new Font("Times New Roman", 0, 14)); // NOI18N
+    
+        jTextField3.setFont(new Font("Times New Roman", 0, 14)); // NOI18N
+       
+        jPasswordField2.setFont(new Font("Times New Roman", 0, 14)); // NOI18N
+    
+        jPasswordField3.setFont(new Font("Times New Roman", 0, 14)); // NOI18N
+    
+        jLabel6.setFont(new Font("Times New Roman", 0, 16)); // NOI18N
+        jLabel6.setText("Password");
+
+        jButton3.setText("Sign Up");
+        jButton3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(33, 33, 33)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jTextField3, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel4)
+                                    .addComponent(jLabel5)
+                                    .addComponent(jLabel6))
+                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(18, 18, 18)
+                                        .addComponent(jPasswordField3, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE))
+                                    .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jTextField2, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(jPasswordField2, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(36, Short.MAX_VALUE))
+            .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton3, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE)
+                .addGap(78, 78, 78))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField3, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(jTextField2, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
+                .addGap(21, 21, 21)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(jPasswordField2, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel6))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(jPasswordField3, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5))
+                .addGap(35, 35, 35)
+                .addComponent(jButton3, GroupLayout.PREFERRED_SIZE, 43, GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        pack();
+    }
+    
+     private Connection connect(){
+        String url = "jdbc:sqlite:C:\\Users\\user\\Desktop\\dataBase\\src\\db\\data.db";
+        Connection con = null;
+        try{
+            Class.forName("org.sqlite.JDBC");
+            con = DriverManager.getConnection(url); 
+        } catch (ClassNotFoundException | SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return con;
+    }
+
+    private void jButton3ActionPerformed(ActionEvent evt) {                                         
+        String password = new String(jPasswordField2.getPassword());
+        String password2 = new String(jPasswordField3.getPassword());
+        // incomplete data
+        if (jTextField2.getText().equals("") || jTextField3.getText().equals("") ||
+            password.equals("") || password2.equals("")){
+            JOptionPane.showMessageDialog(this, "Incomplete data", "", JOptionPane.ERROR_MESSAGE);
+        }
+        // password doesn't match
+        else if (!password.equals(password2)){
+            JOptionPane.showMessageDialog(null, "Password does not Match", "", JOptionPane.ERROR_MESSAGE);
+        }
+        else {
+            String sql = "INSERT INTO ACCOUNTS (name, username, password) VALUES (?, ?, ?)";
+            try (Connection con = connect();
+                PreparedStatement pstmt = con.prepareStatement(sql)){
+                pstmt.setString(1, jTextField3.getText());
+                pstmt.setString(2, jTextField2.getText());
+                pstmt.setString(3, new String(jPasswordField2.getPassword()));
+                pstmt.executeUpdate();
+                con.close();
+                pstmt.close();
+            } catch (SQLException e){
+                String message = e.getMessage();
+                int start  = message.indexOf("(");
+                int end  = message.indexOf(")");
+                String errMessage = message.substring(start + 1, end);
+                System.out.println(message);
+                JOptionPane.showMessageDialog(null, errMessage, "", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            JOptionPane.showMessageDialog(null, "Successful Sign Up");
+            this.dispose();
+        }
+    }                                        
+
+//    public static void main(String args[]) {
+////        EventQueue.invokeLater(new Runnable() {
+////            public void run() {
+//                new SignUp().setVisible(true);
+////            }
+////        });
+//    }
+
+    
+    
+}
